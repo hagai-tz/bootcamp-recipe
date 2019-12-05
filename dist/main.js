c04bfc17c873e590af955edc310b4be5358842f5
hagai-tz <52886733+hagai-tz@users.noreply.github.com>
2019-12-05 11:35:46 +0200

    Finished // 05 - Client Setup

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+
+
+
+// 05 - Client Setup
+$("#ingredient-btn").on("click", function () {
+    let ingredientInput = $("#ingredient-input").val()
+    console.log(ingredientInput)
+
+    $.get(`/recipes/${ingredientInput}`, function (ingredientRecipe) {
+        console.log(ingredientRecipe)
+        // renderTeam(teamData)
+    })
+
+})
